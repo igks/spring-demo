5af3e549768082947cf5affa134dfca30208565a
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-10-25 20:55:17 +0700

    add background job with jobrunr

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .antMatchers("/job/**").permitAll()
         .antMatchers("/socket/**").permitAll()
         .antMatchers("/ws/**").permitAll()
         .antMatchers("/api/**").permitAll()
---
+++ src/main/java/com/example/demo/controllers/JobController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controllers;
+
+import org.jobrunr.scheduling.JobScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.services.JobService;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+  @Autowired
+  private JobScheduler jobScheduler;
+
+  @Autowired
+  private JobService jobService;
+
+  @GetMapping("/run")
+  public String runJob(
+      @RequestParam(value = "name", defaultValue = "Hello World") String name) {
+
+    jobScheduler.enqueue(() -> jobService.execute(name));
+    return "Job is enqueued.";
+
+  }
+
+  @GetMapping("/schedule")
+  public String scheduleJob(
+      @RequestParam(value = "name", defaultValue = "Hello World") String name,
+      @RequestParam(value = "when", defaultValue = "PT3H") String when) {
+
+    jobScheduler.schedule(
+        Instant.now().plus(Duration.parse(when)),
+        () -> jobService.execute(name));
+
+    return "Job is scheduled.";
+  }
+
+}
